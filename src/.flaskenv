326511dcf2888e472e9d13d1e953c5deca8ab593
duilio.mamani@gmail.com <duilio.mamani@gmail.com>
2023-10-31 01:39:50 -0300

    Dockerfile

--- src/.flaskenv
+++ src/.flaskenv
@@ -1,5 +1,3 @@
 # .flaskenv
 FLASK_APP = "server"
 FLASK_ENV = "development"
-FLASK_RUN_HOST = 'localhost'
-FLASK_RUN_PORT = '5173'
--- src/server.py
+++ src/server.py
@@ -1,12 +1,14 @@
 from app import create_app
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv(dotenv_path='.flaskenv')
+load_dotenv(dotenv_path=".flaskenv")
 
 app = create_app()
+
 
 def main():
     app.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
